761ccbcf29486ea689be209225e6fc5f50d0f301
Stanislav Naravetskas <stanislav.naravetskas@arcadia.spb.ru>
2022-05-03 17:25:31 +0300

    tested rxjs features a bit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core'
+import {filter, interval, map, Subscription} from 'rxjs'
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  // styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'angular-lessons';
+  title = 'angular-lessons'
+  sub: Subscription
+
+  constructor() {
+    const intervalStream$ = interval(1000)
+
+    this.sub = intervalStream$
+      .pipe(
+        filter(value => value % 2 === 0),
+        map((value) => `mapped to value ${value}`)
+      )
+      .subscribe((value) => {
+        console.log(value)
+      })
+  }
+
+  stop() {
+    this.sub.unsubscribe()
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,10 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { PostComponent } from './post/post.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    PostComponent
+    AppComponent
   ],
   imports: [
     BrowserModule
